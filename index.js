bf1ccab3892804498a0eb25ed7a7d722516c1c9f
imurx <urielfontan2002@gmail.com>
2018-12-26 13:52:28 -0300

    finished all the things

--- index.js
+++ index.js
@@ -12,36 +12,43 @@ process.on("unhandledrejection", (promiseRejectionEvent) => {
 
 client.on("ready", () => {
     logger.log("Bot is ready");
+    setInterval(() => {
+        const guild = client.guilds.first();
+        guild.channels.get(settings.channelForOnlineMembers).setName(`Member Count: ${guild.memberCount}`);
+        guild.channels.get(settings.channelForBots).setName(`Bot Count: ${guild.members.filter(member => member.user.bot).size}`);
+    });
 });
 
 client.on("guildMemberAdd", async (member) => {
     let dmsDisabled = false;
     for(let i = 0; i<3; i++) {
         const { buffer, res } = makeARandomImage();
-        let channel = dmsDisabled ? member.guild.channels.get(settings.channel) : await member.createDM();
-        await channel.send(`Are you a bot?\n\`\`Retries left: ${3-i}\`\``, { files: [buffer] })
+        let channel = dmsDisabled ? member.guild.channels.get(settings.channelForDisabledDM) : await member.createDM();
+        await channel.send((dmsDisabled ? `<@${member.id}>` : "") + `Are you a bot?\n\`\`Retries left: ${3-i}\`\``, { files: [buffer] })
             .catch(() => { 
                 if(channel.type === "dm") {
                     dmsDisabled = true;
-                    channel = member.guild.channels.get(settings.channel);
-                    channel.send(`Your DMs are disabled so I will send the captchas through here. Are you a bot?\n\`\`Retries left: ${3-i}\`\``, { files: [buffer] });
+                    channel = member.guild.channels.get(settings.channelForDisabledDM);
+                    channel.send(`<@${member.id}> Your DMs are disabled so I will send the captchas through here. Are you a bot?\n\`\`Retries left: ${3-i}\`\``, { files: [buffer] });
                 }
             });
         const m = await channel.awaitMessages(m => !isNaN(m.content), { max: 1, time:30000, errors: ["time"] })
             .then(collected => collected.first())
             .catch(collected => collected.first());
         if(m == undefined) {
-            await channel.send("Timeout! You took too much time in responding to an easy math question.");
+            await channel.send((dmsDisabled ? `<@${member.id}>` : "") + "Timeout! You took too much time in responding to an easy math question.");
         } else if(m.content == res) {
-            await channel.send("Success! You got the answer right, you must have a very high IQ!");
+            await channel.send((dmsDisabled ? `<@${member.id}>` : "") + "Success! You got the answer right, you must have a very high IQ!");
             logger.log(`The user ${member.user.tag}(${member.user.id}) solved the captchas after ${i+1} try/tries`);
+            member.roles.add(settings.giveRole);
+            member.roles.delete(settings.removeRole);
             return;
         } else {
             await channel.send("Failure... that answer isn't right.");
         }
         continue;
     }
-    await member.send("You are going to be kicked out of the server because you failed solving the captchas. RIP");
+    await member.send("You are going to be kicked out of the server because you failed solving the captchas. RIP").catch(() => {if(dmsDisabled) logger.warn(`oh ${member.user.tag}(${member.user.id}) had DMs disabled rip`);});
     await member.kick("Failed to solve the captchas");
     logger.log(`Kicked the user ${member.user.tag}(${member.user.id})`);
 });
@@ -50,7 +57,7 @@ function makeARandomImage() {
     const canvas = createCanvas(420,124);
     const ctx = canvas.getContext("2d");
 
-    const firstNum = randomNumber(51), secondNum = randomNumber(51);
+    const firstNum = randomNumber(settings.maxMath), secondNum = randomNumber(settings.maxMath);
     const res = firstNum + secondNum;
 
     ctx.fillStyle = "rgba(255,255,255,1)";
