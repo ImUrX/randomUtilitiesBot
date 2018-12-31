7cff05b9a62476a8836f0332cb09c6ac4b89db8c
imurx <urielfontan2002@gmail.com>
2018-12-30 22:34:15 -0300

    and forgot that guilds is a map which returns an array in this case of for...of..

--- tasks/channelMemberCounter.js
+++ tasks/channelMemberCounter.js
@@ -4,7 +4,7 @@ module.exports = class extends Task {
 
     async run() {
         this.client.emit("log", "Changing the numbers of the member counter for each guild...");
-        for(const guild of this.client.guilds) {
+        for(const [,guild] of this.client.guilds) {
             const channelForMembers = guild.settings.get("channelForMembers");
             if(!channelForMembers || !guild.me.hasPermission("MANAGE_CHANNELS")) continue;
             guild.channels.get(channelForMembers).setName(`Member Count: ${guild.memberCount}`);
